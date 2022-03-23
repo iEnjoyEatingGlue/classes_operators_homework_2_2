087d4de8cd31ab30488934730b668a6f2316305a
iEnjoyEatingGlue <gumadojedzenia@gmail.com>
2022-03-23 22:16:50 +0100

    dziala

--- homework/main.cpp
+++ homework/main.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 #include <string>
 
-
-
 class Item
 {
 private:
@@ -21,44 +19,56 @@ public:
         Amount_sold = amount;
     }
 
-    friend std::ostream& operator<<(std::ostream& stream,const Item &something);
+    std::string dumb_af_0 (){return Name ;}
+    int dumb_af_1 (Item abc){return abc.VAT_tax_type;}
+    float dumb_af_2 (Item abc){return abc.Unit_net_price;}
+    float dumb_af_3 (Item abc){return abc.Amount_sold;}
+
 };
 
 class Invoice
 {
 private:
-    int seller;
-    int buyer;
+    unsigned long long int seller;
+    unsigned long long int buyer;
     std::vector <Item> items;
 
 public:
-    Invoice(int sel, int buy/*std::vector <Item> item*/)
+    Invoice(unsigned long long int sel, unsigned long long int buy)
     {
         seller = sel;
         buyer = buy;
-        /*items.emplace_back(item);*/
     }
 
-    void add_item(std::vector <Item> item)
+    void add_item(Item item)
     {
         items.emplace_back(item);
     }
-    friend std::ostream& operator<<(std::ostream& stream,const Invoice &something);
 
+//    std::string just_dumb_0(Item abc)
+//    {
+
+//    }
+
+    friend std::ostream& operator<<(std::ostream& stream, Invoice &something);
 };
 
-std::ostream& operator<<(std::ostream& stream,const Invoice &something)
+std::ostream& operator<<(std::ostream& stream, Invoice &something)
 {
     stream << "------------------VAT invoice------------------" << std::endl
            << "===============================================" << std::endl
            << "Seller: " << something.seller << "      " << something.buyer << std::endl << std::endl
-           << "                  c.j. VAT   il.    net   total" << std::endl;
-    return(stream);
-}
+           << something.items[0].dumb_af_0();
 
+
+
+    return (stream);
+}
 
 int main()
 {
-    return(0);
-
+    Invoice inv(777, 999);
+    inv.add_item(Item("M3 screw", 23, 0.37, 100));
+    inv.add_item(Item("2 mm drill", 8, 2.54, 2));
+    std::cout << inv << std::endl;
 }
